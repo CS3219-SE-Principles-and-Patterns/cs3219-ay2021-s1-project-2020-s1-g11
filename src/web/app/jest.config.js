c3b70eb37b1e4defb4d80cf5a63ed9f8b2b55f89
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-09-26 22:31:46 +0800

    Add frontend test coverage report

--- src/web/app/jest.config.js
+++ src/web/app/jest.config.js
@@ -19,5 +19,9 @@ module.exports = {
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  testURL: 'http://localhost/'
+  testURL: 'http://localhost/',
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{vue,js}', '!**/node_modules/**', '!../../../out/**', '!../../../build/**'],
+  coverageReporters: ['lcov', 'text-summary'],
+  coverageDirectory: '../../../build/test-results/',
 }
