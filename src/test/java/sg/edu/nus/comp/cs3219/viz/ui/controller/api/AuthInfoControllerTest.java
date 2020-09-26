790b576921c7e61f0baa1af5db8e6578b6ff7e99
Ahmed Bahajjaj <Ahmed_Bahajjaj@u.nus.edu>
2020-09-27 00:33:39 +0800

    Add coveralls test coverage (#11)
    * Add frontend test coverage report
    
    * Add backend test coverage report
    
    * Add coveralls submission for frontend
    
    * Add parallel coveralls submission

---
+++ src/test/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/AuthInfoControllerTest.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.comp.cs3219.viz.ui.controller.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthInfoControllerTest {
+
+    @Test
+    void getAuthInfo() {
+        assertTrue(true);
+    }
+}
