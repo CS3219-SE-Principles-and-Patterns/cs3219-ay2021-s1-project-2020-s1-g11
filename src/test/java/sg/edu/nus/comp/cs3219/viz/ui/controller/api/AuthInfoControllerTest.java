baa6e95abdc14dd27d92a1354513a9c13100d4c2
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-10-02 11:34:27 +0800

    Initial skeleton

--- src/test/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/AuthInfoControllerTest.java
+++ src/test/java/sg/edu/nus/comp/cs3219/viz/ui/controller/api/AuthInfoControllerTest.java
@@ -1,9 +1,14 @@
 package sg.edu.nus.comp.cs3219.viz.ui.controller.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import sg.edu.nus.comp.cs3219.viz.VizApplication;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(classes = VizApplication.class)
+@AutoConfigureMockMvc
 class AuthInfoControllerTest {
 
     @Test
