1b01e736473b372526503aa7fb983b1877ea2c99
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-10-16 16:05:40 +0800

    Add routing and view skeleton

--- src/web/app/src/components/MenuBar.vue
+++ src/web/app/src/components/MenuBar.vue
@@ -11,6 +11,10 @@
       <template slot="title"> 
         <i class="el-icon-user-solid"></i> {{ userNickname }}
       </template>
+      <el-menu-item index="/data" :disabled="!isLogin">
+        <i class="el-icon-files"></i>
+        My Data
+      </el-menu-item>
       <el-menu-item index="/analyze" :disabled="!isLogin">
         <i class="el-icon-data-analysis"></i>
         My Presentations
---
+++ src/web/app/src/views/ManageData.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-main>
+    <el-alert
+        title="You need to login-in to view the page"
+        type="error"
+        v-if="!isLogin && !isAppLoading">
+      &nbsp;<el-button type="warning" plain size="mini" @click="navigateToHomePage">Return to the Home Page</el-button>
+    </el-alert>
+    <el-card v-if="isDataListEmpty" >
+      <EmptyData />
+    </el-card>
+  </el-main>
+</template>
+
+<script>
+import EmptyData from "@/components/emptyStates/EmptyData";
+export default {
+  name: "ManageData",
+  components: {EmptyData},
+  computed: {
+    isLogin() {
+      return this.$store.state.userInfo.isLogin;
+    },
+    isAppLoading: function () {
+      return this.$store.state.isPageLoading || this.$store.state.dbMetaData.entitiesStatus.isLoading;
+    },
+    isDataListEmpty() {
+      // return this.$store.state.presentation.presentationList.length <= 0;
+      return true;
+    },
+  },
+  methods: {
+    navigateToHomePage() {
+      this.$router.replace("/home");
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
