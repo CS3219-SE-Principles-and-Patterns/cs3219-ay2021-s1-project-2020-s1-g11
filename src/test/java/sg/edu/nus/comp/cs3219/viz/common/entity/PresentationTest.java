f9da85fdf1c81944949a99b95e2a944057e1e4c1
Ahmed Bahajjaj <Ahmed_Bahajjaj@u.nus.edu>
2020-10-18 11:00:35 +0800

    Add entity test skeletons (#45)
    * Add VersionTest cases
    
    * Add template for entity tests

---
+++ src/test/java/sg/edu/nus/comp/cs3219/viz/common/entity/PresentationTest.java
@@ -0,0 +1,48 @@
+package sg.edu.nus.comp.cs3219.viz.common.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PresentationTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getVersion() {
+    }
+
+    @Test
+    void setVersion() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void getCreatorIdentifier() {
+    }
+
+    @Test
+    void setCreatorIdentifier() {
+    }
+}
---
+++ src/test/java/sg/edu/nus/comp/cs3219/viz/common/entity/record/SubmissionAuthorRecordTest.java
@@ -0,0 +1,37 @@
+package sg.edu.nus.comp.cs3219.viz.common.entity.record;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubmissionAuthorRecordTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getDataSet() {
+    }
+
+    @Test
+    void setDataSet() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+}
