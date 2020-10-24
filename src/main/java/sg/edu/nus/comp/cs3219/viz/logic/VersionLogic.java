31cf564b524d1b90506ef48368f53173a4d5404e
Jhocx <jhocx@u.nus.edu>
2020-10-24 18:38:53 +0800

    Version APIs

--- src/main/java/sg/edu/nus/comp/cs3219/viz/logic/VersionLogic.java
+++ src/main/java/sg/edu/nus/comp/cs3219/viz/logic/VersionLogic.java
@@ -33,4 +33,44 @@ public class VersionLogic {
         return versionRepository.save(newVersion);
     }
 
+    public Version saveForUser(String versionId, String dataSet) {
+        Version newVersion = new Version();
+        Version.VersionPk newVersionPk = new Version.VersionPk();
+        newVersionPk.setVersionId(versionId);
+        newVersionPk.setDataSet(dataSet);
+        newVersion.setId(newVersionPk);
+
+        return versionRepository.save(newVersion);
+    }
+
+    public Boolean editVersionId(String dataSet, String old_versionId, String new_versionId) {)
+        Version.VersionPk new_versionPk = new Version.VersionPk();
+        new_versionPk.setVersionId(new_versionId);
+        new_versionPk.setDataSet(dataSet);
+        if (versionRepository.existsById(new_versionPk)) {
+            return false;
+        } 
+        
+        Version.VersionPk old_versionPk = new Version.VersionPk();
+        old_versionPk.setVersionId(old_versionId);
+        old_versionPk.setDataSet(dataSet);
+        Version versionToEdit;
+        
+        try {
+            versionToEdit = versionRepository.getOne(old_versionPk);
+        } catch(Exception e) {
+            return false;
+        }
+
+        versionToEdit.setId(new_versionPk);
+        return true;
+    }
+
+    public Boolean deleteVersion(String dataSet, String versionId) {
+        Version.VersionPk versionPk = new Version.VersionPk();
+        versionPk.setVersionId(versionId);
+        versionPk.setDataSet(dataSet);
+        versionRepository.deleteById(versionPk);
+        return true;
+    }
 }
