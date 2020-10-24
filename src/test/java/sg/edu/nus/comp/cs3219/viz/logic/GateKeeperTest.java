a77a09838a66c21788970babe21ddd3c413d20ce
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-10-24 14:23:12 +0800

    Add FileHelper

--- src/test/java/sg/edu/nus/comp/cs3219/viz/logic/GateKeeperTest.java
+++ src/test/java/sg/edu/nus/comp/cs3219/viz/logic/GateKeeperTest.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.comp.cs3219.viz.logic;
 
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,12 +9,21 @@ import sg.edu.nus.comp.cs3219.viz.VizApplication;
 import sg.edu.nus.comp.cs3219.viz.storage.repository.PresentationAccessControlRepository;
 import sg.edu.nus.comp.cs3219.viz.test.BaseComponentTest;
 
+/**
+ * SUT: {@link GateKeeper}
+ */
 @SpringBootTest(classes = VizApplication.class)
 @AutoConfigureMockMvc
 class GateKeeperTest extends BaseComponentTest {
 
     @Autowired
-    private static PresentationAccessControlRepository pacr;
+    private static PresentationAccessControlRepository repo;
 
-    private static GateKeeper gateKeeper = new GateKeeper(pacr);
+    private static final GateKeeper gateKeeper = new GateKeeper(repo);
+    private final JSONObject data = loadDataBundle("GateKeeperTest.json");
+
+    @BeforeEach
+    public void setUp() {
+        gaeSimulation.loginUser("user@test.com");
+    }
 }
--- src/test/java/sg/edu/nus/comp/cs3219/viz/test/BaseComponentTest.java
+++ src/test/java/sg/edu/nus/comp/cs3219/viz/test/BaseComponentTest.java
@@ -1,10 +1,28 @@
 package sg.edu.nus.comp.cs3219.viz.test;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
+import java.io.IOException;
+
 public class BaseComponentTest {
     protected static final GaeSimulation gaeSimulation = GaeSimulation.inst();
+
+    protected JSONObject loadDataBundle(String jsonFileName) {
+        try {
+            String pathToJsonFile = getTestDataFolder() + jsonFileName;
+            String jsonString = FileHelper.readFile(pathToJsonFile);
+            return new JSONObject(jsonString);
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getTestDataFolder() {
+        return "src/test/resources/data";
+    }
 
     @BeforeClass
     public static void setUpGae() {
